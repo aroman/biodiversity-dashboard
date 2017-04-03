c9ebf9f1de19629188661ec03ebf603158c01a8a
Avi Romanoff <avi@romanoff.me>
2017-04-03 10:40:42 -0400

    Human Impact sparklines

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,19 +1,57 @@
 import React, { Component } from 'react';
-import './Dashboard.css';
+import * as SL from 'react-sparklines';
 
 import TourismIcon from './tourism.svg'
 import JobGrowthIcon from './job-growth.svg'
 import HouseholdIncomeIcon from './household-income.svg'
 import ExportsIcon from './exports.svg'
 
+import './Dashboard.css';
+
 const RED = '#FD9393'
 const GREEN = '#1DCC92'
+const INTERVAL = 1000 // milliseconds
 
+
+function boxMullerRandom () {
+    let phase = false,
+        x1, x2, w, z;
+
+    return (function() {
+
+        if (phase = !phase) {
+            do {
+                x1 = 2.0 * Math.random() - 1.0;
+                x2 = 2.0 * Math.random() - 1.0;
+                w = x1 * x1 + x2 * x2;
+            } while (w >= 1.0);
+
+            w = Math.sqrt((-2.0 * Math.log(w)) / w);
+            return x1 * w;
+        } else {
+            return x2 * w;
+        }
+    })();
+}
+
+function randomData(n = 30) {
+    return Array.apply(0, Array(n)).map(boxMullerRandom);
+}
 
 class HumanImpactPanel extends Component {
 
+  constructor(props) {
+      super(props);
+      this.state = { data: randomData(20) };
+      setInterval(() =>
+        this.setState({
+            data: this.state.data.concat([boxMullerRandom()])
+        }), INTERVAL);
+  }
+
   render() {
-    const {label, icon, percentage, units, value} = this.props
+    let {label, icon, percentage, units, value} = this.props
+    percentage = this.state.data.slice(-1).pop()
     const statsColor = (percentage > 0) ? GREEN : RED
     const statsSymbol = (percentage > 0) ? '▲' : '▼'
     const statsPercentage = (percentage > 0) ? percentage : -percentage
@@ -26,12 +64,16 @@ class HumanImpactPanel extends Component {
           <div className="Impact-panel-header-label">{label}</div>
       </div>
       <div className="Impact-panel-stats">
-        <div className="Impact-panel-stats-percentage" style={{color:statsColor}}>{statsSymbol} {statsPercentage}%</div>
+        <div className="Impact-panel-stats-percentage" style={{color:statsColor}}>{statsSymbol} {statsPercentage.toFixed(1)}%</div>
         <div className="Impact-panel-stats-value-container">
           <div className="Impact-panel-stats-units">{units}</div>
           <div className="Impact-panel-stats-value" style={{color:statsColor}}>{value}</div>
         </div>
       </div>
+      <SL.Sparklines data={this.state.data} limit={20}>
+          <SL.SparklinesLine />
+          <SL.SparklinesSpots />
+      </SL.Sparklines>
     </div>
     )
   }
@@ -43,10 +85,10 @@ class Dashboard extends Component {
     return (
       <div className="Dashboard">
         <div className="Column Column-center">
-          <div className="Column-title">Water Health</div>
+          <div className="Column-title">Indicator Species</div>
         </div>
         <div className="Column Column-left">
-          <div className="Column-title">Indicator Species</div>
+          <div className="Column-title">Water Health</div>
         </div>
         <div className="Column Column-right">
           <div className="Column-title">Human Impact</div>
