6b99997bfe44a92507f20fc04698a1cdcc04ad28
Avi Romanoff <avi@romanoff.me>
2017-04-03 10:05:30 -0400

    Add HumanImpactPanel

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,6 +1,27 @@
 import React, { Component } from 'react';
 import './Dashboard.css';
 
+import TourismIcon from './tourism.svg'
+import JobGrowthIcon from './job-growth.svg'
+import HouseholdIncomeIcon from './household-income.svg'
+import ExportsIcon from './exports.svg'
+
+class HumanImpactPanel extends Component {
+  render() {
+    const {label, icon} = this.props
+    return (
+      <div className="Impact-panel">
+        <div className="Impact-panel-header">
+          <div className="Impact-panel-header-icon">
+            <img src={icon} alt={label}/>
+          </div>
+          <div className="Impact-panel-header-label">{label}</div>
+      </div>
+    </div>
+    )
+  }
+}
+
 class Dashboard extends Component {
   render() {
     return (
@@ -13,6 +34,10 @@ class Dashboard extends Component {
         </div>
         <div className="Column Column-right">
           <div className="Column-title">Human Impact</div>
+          <HumanImpactPanel label="Exports" icon={ExportsIcon}/>
+          <HumanImpactPanel label="Household Income" icon={HouseholdIncomeIcon}/>
+          <HumanImpactPanel label="Job Growth" icon={JobGrowthIcon}/>
+          <HumanImpactPanel label="Tourism" icon={TourismIcon}/> 
         </div>
       </div>
     );
